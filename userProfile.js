b2225152391212d29e75e35237bd4d6ab487ecf5
Piotr Warząchowski <108893195+PiotrWarzachowski@users.noreply.github.com>
2022-07-07 20:43:45 +0200

    Changed storage of infouser to APPDATA

--- userProfile.js
+++ userProfile.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 const db = require('./dbEncryption')
 
 const createUserProfile = () => {
-    if (!fs.existsSync('./core')){
-        fs.mkdirSync('./core');
+    if (!fs.existsSync(`${process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")}/userprofile.json'`)){
         const fileData = db.encrypt(`{"username" : "","hasPrivateKey" : false,"privateKey" : "","transactionsSent" : {"bitcoin" : 0,"ethereum" : 0}}`)
-        fs.writeFileSync('./core/userprofile.json', JSON.stringify(fileData), function (err) {
+        fs.writeFileSync(`${process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")}/userprofile.json'`, JSON.stringify(fileData), function (err) {
             if (err) throw err;
         });
     }
@@ -14,31 +13,32 @@ const createUserProfile = () => {
 const updateUserProfile = (key, value) => {
 
     
-    let cryptedContent = JSON.parse(fs.readFileSync('./core/userprofile.json', 'utf8'));
+    let cryptedContent = JSON.parse(fs.readFileSync(`${process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")}/userprofile.json'`, 'utf8'));
     let content = JSON.parse(db.decrypt(cryptedContent));
     if (key != "privatekey"){
         content[key] = value;
-        fs.writeFileSync('./core/userprofile.json', JSON.stringify(content));
+        fs.writeFileSync(`${process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")}/userprofile.json'`, JSON.stringify(content));
     }
 }
 
 const addPrivateKey = (privateKey) => {
-    let cryptedContent = JSON.parse(fs.readFileSync('./core/userprofile.json', 'utf8'));
+    let cryptedContent = JSON.parse(fs.readFileSync(`${process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")}/userprofile.json'`, 'utf8'));
     let content = JSON.parse(db.decrypt(cryptedContent));
     content.privateKey = privateKey;
     content.hasPrivateKey = true;
     let cryptedNewContent = db.encrypt(JSON.stringify(content));
-    fs.writeFileSync('./core/userprofile.json', JSON.stringify(cryptedNewContent));
+    fs.writeFileSync(`${process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")}/userprofile.json'`, JSON.stringify(cryptedNewContent));
 }
 
 // Function for developers to see decrypted database
 
 const readDatabase = () => {
-    let cryptedContent = JSON.parse(fs.readFileSync('./core/userprofile.json', 'utf8'));
+    let cryptedContent = JSON.parse(fs.readFileSync(`${process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")}/userprofile.json'`, 'utf8'));
     let content = JSON.parse(db.decrypt(cryptedContent));
     console.log(content)
 }
-
+createUserProfile()
+readDatabase()
 module.exports = {
     createUserProfile,
     updateUserProfile,
