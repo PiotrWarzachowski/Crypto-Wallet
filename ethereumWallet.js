03b22dacdae4653d737ba8f761a28031178e95dd
Piotr Warząchowski <108893195+PiotrWarzachowski@users.noreply.github.com>
2022-07-09 19:24:42 +0200

    Update ethereumWallet.js

--- ethereumWallet.js
+++ ethereumWallet.js
@@ -2,7 +2,13 @@ const ethers = require('ethers')
 const profile = require('./userProfile')
 
 const generatEthereumWalletPair = async (privateKey) => {
-
+    
+    /*
+        Retrieving keypair from a privateKey.
+        
+        ~Piotr Warząchowski
+    */
+    
     const wallet = new ethers.Wallet(privateKey)
     return ["0x" + privateKey, wallet.address]
 
